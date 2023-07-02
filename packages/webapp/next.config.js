0b31502d860281958ad00e29fd8faee427adc230
Milad Nekofar <milad@nekofar.com>
2023-07-02 16:18:24 +0330

    chore(webapp): restructure export and disable image optimization
    The export output is updated to 'export' in next.config.js. Additionally, image optimization is enabled by setting the unoptimized property to true in the images configuration.

--- packages/webapp/next.config.js
+++ packages/webapp/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
   reactStrictMode: false,
-
 
   webpack(config) {
     config.resolve.alias = {
